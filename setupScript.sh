80cfcad9c28a1b1840d1e21f83fc05a3aa2c925a
eitelvolkerts <eitelvolkerts@yandex.com>
2023-11-10 17:52:58 +0300

    fixed the script

--- setupScript.sh
+++ setupScript.sh
@@ -21,6 +21,10 @@ tsc
 DIR=dist node --loader ts-node/esm ./writeVersionData.ts
 node jsongen.js $pk $pass
 mv ./config/main.template.yaml ./config/main.yaml
-printf "#!/usr/bin/env bash\n\nNETWORK_NAME=$6 NETWORK_RPC=$4 AGENT_ADDRESS=$5 KEEPER_WORKER_ADDRESS=$2 KEYPASSWORD=$3 ACCRUE_REWARDS='false' LOG_LEVEL='debug' APP_ENV='service' node $(pwd>chmod a+x startupScript.sh
-printf "[Unit]\nDescription=PowerAgent Noderunner\nWants=network-online.target\nAfter=network-online.target\n\n[Service]\nRestart=always\nExecStart=$(pwd)/startupScript.sh\n\n[Install]\nWa>chmod 644 /etc/systemd/system/PowerAgent.service
+printf "#!/usr/bin/env bash\n\nNETWORK_NAME=$6 NETWORK_RPC=$4 AGENT_ADDRESS=$5 KEEPER_WORKER_ADDRESS=$2 KEYPASSWORD=$3 ACCRUE_REWARDS='false' LOG_LEVEL='debug' APP_ENV='service' node $(pwd)/dist/Cli.js" > startupScript.sh
+chmod a+x startupScript.sh
+printf "[Unit]\nDescription=PowerAgent Noderunner\nWants=network-online.target\nAfter=network-online.target\n\n[Service]\nRestart=always\nExecStart=$(pwd)/startupScript.sh\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/PowerAgent.service
+chmod 644 /etc/systemd/system/PowerAgent.service
 systemctl enable --now PowerAgent.service
+
+
